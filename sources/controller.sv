501f20bb6feb850900db3e55e3ddf1faddf7602f
srjsrini <srjsrini@gmail.com>
2026-03-06 13:28:49 +0000

    add uart golden

--- sources/controller.sv
+++ sources/controller.sv
@@ -1,23 +1,114 @@
-`timescale 1us/1us
+module uart_tx (
+    input  logic clk,
+    input  logic rst,
+    input  logic tx_start,
+    input  logic [7:0] data_in,
+    input  logic [15:0] baud_div,
+    input  logic parity_en,
+    input  logic parity_type,
 
-module controller (
-    input  logic clk, rst_n,
-    input  logic [4:0] rs1_addr, rs2_addr, rd_addr_exec,
-    input  logic rs1_en, rs2_en, we_exec,
-    output logic stall, flush_exec
+    output logic tx,
+    output logic busy
 );
 
-    // BUGGY IMPLEMENTATION: 
-    // This will pass the RAW hazard test, but fail the "No Hazard" 
-    // and "Register 0" tests because it stalls too often.
-    always_comb begin
-        if (rst_n && we_exec) begin
-            stall = 1'b1;
-            flush_exec = 1'b1;
-        end else begin
-            stall = 1'b0;
-            flush_exec = 1'b0;
+logic [3:0] bit_cnt;
+logic [7:0] shift_reg;
+logic [15:0] baud_cnt;
+logic parity_bit;
+
+typedef enum logic [2:0] {
+    IDLE,
+    START,
+    DATA,
+    PARITY,
+    STOP
+} state_t;
+
+state_t state;
+
+always_ff @(posedge clk) begin
+    if (rst) begin
+        state <= IDLE;
+        tx <= 1;
+        busy <= 0;
+        baud_cnt <= 0;
+        bit_cnt <= 0;
+    end else begin
+
+        case(state)
+
+        IDLE: begin
+            tx <= 1;
+            busy <= 0;
+
+            if(tx_start) begin
+                shift_reg <= data_in;
+                parity_bit <= ^data_in;  
+
+                if(parity_type)
+                    parity_bit <= ~(^data_in);
+
+                state <= START;
+                busy <= 1;
+                baud_cnt <= 0;
+            end
         end
+
+        START: begin
+            tx <= 0;
+
+            if(baud_cnt == baud_div) begin
+                baud_cnt <= 0;
+                bit_cnt <= 0;
+                state <= DATA;
+            end else
+                baud_cnt <= baud_cnt + 1;
+        end
+
+        DATA: begin
+            tx <= shift_reg[0];
+
+            if(baud_cnt == baud_div) begin
+                baud_cnt <= 0;
+                shift_reg <= shift_reg >> 1;
+
+                if(bit_cnt == 7) begin
+                    if(parity_en)
+                        state <= PARITY;
+                    else
+                        state <= STOP;
+                end
+                else
+                    bit_cnt <= bit_cnt + 1;
+            end
+            else
+                baud_cnt <= baud_cnt + 1;
+        end
+
+        PARITY: begin
+            tx <= parity_bit;
+
+            if(baud_cnt == baud_div) begin
+                baud_cnt <= 0;
+                state <= STOP;
+            end
+            else
+                baud_cnt <= baud_cnt + 1;
+        end
+
+        STOP: begin
+            tx <= 1;
+
+            if(baud_cnt == baud_div) begin
+                baud_cnt <= 0;
+                state <= IDLE;
+            end
+            else
+                baud_cnt <= baud_cnt + 1;
+        end
+
+        endcase
     end
+end
 
 endmodule
