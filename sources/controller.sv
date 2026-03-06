869fd26b746ef5ee65839d334d39a8a7c62ee6b4
srjsrini <srjsrini@gmail.com>
2026-03-06 11:49:22 +0000

    Update baseline to buggy

--- sources/controller.sv
+++ sources/controller.sv
@@ -1,17 +1,23 @@
 `timescale 1us/1us
+
 module controller (
     input  logic clk, rst_n,
     input  logic [4:0] rs1_addr, rs2_addr, rd_addr_exec,
     input  logic rs1_en, rs2_en, we_exec,
     output logic stall, flush_exec
 );
+
+    // BUGGY IMPLEMENTATION: 
+    // This will pass the RAW hazard test, but fail the "No Hazard" 
+    // and "Register 0" tests because it stalls too often.
     always_comb begin
-        stall = 1'b0; flush_exec = 1'b0;
-        if (rst_n && we_exec && (rd_addr_exec != 5'b0)) begin
-            if ((rs1_en && (rs1_addr == rd_addr_exec)) || 
-                (rs2_en && (rs2_addr == rd_addr_exec))) begin
-                stall = 1'b1; flush_exec = 1'b1;
-            end
+        if (rst_n && we_exec) begin
+            stall = 1'b1;
+            flush_exec = 1'b1;
+        end else begin
+            stall = 1'b0;
+            flush_exec = 1'b0;
         end
     end
+
 endmodule
